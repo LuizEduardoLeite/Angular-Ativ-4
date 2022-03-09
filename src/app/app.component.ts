07f7bf5a2de0ca987c933c319bb9440db9362cd1
luiz eduardo <luizeduardomelo.leite@gmail.com>
2022-03-09 20:35:38 -0300

    angular-ativ-4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import  { MatSnackBar } from '@angular/material/snack-bar';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -10,14 +11,7 @@ export class AppComponent {
   
   constructor(private snackBar: MatSnackBar) {}
 
-  openSnackBar1() {
-    this.snackBar.open('Voce escolheu o plano Básico','fechar');
-  }
-  openSnackBar2() {
-    this.snackBar.open('Voce escolheu o plano Profissional','fechar');
-  }
-  openSnackBar3() {
-    this.snackBar.open('Voce escolheu o plano Mestre','fechar');
-  }
-  
+  openSnackBar() {
+    this.snackBar.open('Voce escolheu este plano.','fechar');
+  }  
 }
